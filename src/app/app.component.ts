1e18e2aa6bfcfa2e4b18c49b1914ee321e523fbd
Isa Giongo <isagiongo@gmail.com>
2019-07-21 18:21:54 -0300

    Cria componente photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-  description = 'Gato deitado';
-  url = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80';
 }
