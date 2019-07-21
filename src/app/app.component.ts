bcd24190a076634ba96449a7fa966caf9ee4181a
Isa Giongo <isagiongo@gmail.com>
2019-07-21 17:44:59 -0300

    inicia projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description = 'Gato deitado';
+  url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFfx3XvNkTuwrSjBmvBQUDbS4fqCDRdPALbgZ77mGW0IANd12R';
 }
